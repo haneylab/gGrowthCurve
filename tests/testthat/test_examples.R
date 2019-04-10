afbfe51b64d560a5e9d7b83009eaf300abf1d011
Andrew Wilson <andrew.wilson849@icloud.com>
2019-04-10 15:35:24 -0700

    added more tests for example data

--- tests/testthat/test_examples.R
+++ tests/testthat/test_examples.R
@@ -1,5 +1,11 @@
 context("Example Data")
 
-testthat::test_that("gGrowthCurve example data exists in inst/extdata/", {
-  testthat::expect_equal(length(gGrowthCurve::gGrowthCurve_example_dir()), 1)
+testthat::test_that("gGrowthCurve inst/extdata/ contains the correct number of files", {
+  testthat::expect_equal(length(gGrowthCurve::gGrowthCurve_example()), 2)
+})
+
+testthat::test_that("The files in /extdata/ are correct", {
+  dir <- gGrowthCurve::gGrowthCurve_example_dir()
+  files <- c("/example_GrowthCurve.csv", "/metadata.csv")
+  testthat::expect_true(all(file.exists(paste0(dir, files))))
 })
